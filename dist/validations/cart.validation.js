bf2d9d34dfca0ae2f658820f028454a391e4c862
Muhammad Marwan <84658917+muhammadmarwan@users.noreply.github.com>
2025-07-17 05:50:54 +0400

    Revert "added build command"
    This reverts commit f69b2ae2f699d27c439abd0017957f67186cda77.

--- dist/validations/cart.validation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addToCartSchema = void 0;
-const zod_1 = require("zod");
-exports.addToCartSchema = zod_1.z.object({
-    productId: zod_1.z.string().nonempty('Product ID is required'),
-    quantity: zod_1.z.number().int().positive('Quantity must be a positive integer'),
-});
